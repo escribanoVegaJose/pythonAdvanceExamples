1369e1438519148fcaf2bc85554e6e46f5857724
Jose Escribano <jescribanov@viewnext.com>
2021-01-08 18:18:36 +0100

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,14 @@
+# función rápida
+
+# estructura -> lambda parámetros : sentencia 
+
+# ejemplo 1 
+res = lambda firstParam , secParam : firstParam * secParam
+print(res(2,"-Las cosas de la vida-"))
+
+# ejemplo 2 lambda dentro de una función 
+def funclambda (text): 
+    return lambda param : param + text
+
+res2 = funclambda('cola')
+print(res2("colita igual viene la"))
